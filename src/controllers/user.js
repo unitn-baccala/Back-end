a381e520de8c504f7ea4a7ce7ecf3869a6540176
circled-square <circled-square@github.com>
2022-12-28 21:28:33 +0100

    aggiunte regex per la validazione di indirizzo email e password in fase di registrazione

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,18 +2,29 @@ const User = require('../models/user');
 
 // newUser function for post user route
 const newUser = async (req, res, next) => {
+    const username = req.query.user, password = req.query.password;
     const user = new User({
-        username: req.query.user, password: req.query.password
+        username, password
     });
 
-    
+    //pw must have 1 lowercase letter, 1 uppercase, 1 number and must be between 8 and 32 characters
+    if (!password.match(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])([^\s]){8,32}$/)) {
+        res.status(400).send("failed to create user: insecure password");
+        return;
+    }
+
+    //email address must be valid
+    if (!username.match(/^([_a-z0-9]+[\._a-z0-9]*)(\+[a-z0-9]+)?@(([a-z0-9-]+\.)*[a-z]{2,4})$/)) {
+        res.status(400).send("failed to create user: invalid email address");
+        return;
+    }
 
     const foundUser = await User.findOne({ username: user.username }).exec();
 
-    if(foundUser === null) {
+    if (foundUser === null) {
         const user = await user.save();
         
-        if(user !== null) {
+        if (user !== null) {
             res.status(201).send("user saved successfully");
         } else {
             res.status(500).send("failed to create user");
