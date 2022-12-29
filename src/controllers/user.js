75da103d0ebed2cda70eed7266fc6b2ed8c9d3e3
circled-square <circled-square@github.com>
2022-12-29 12:59:03 +0100

    changed username to email

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,29 +2,29 @@ const User = require('../models/user');
 
 // newUser function for post user route
 const newUser = async (req, res, next) => {
-    const username = req.query.user, password = req.query.password;
+    const email = req.query.email, password = req.query.password;
     const user = new User({
-        username, password
+        email, password
     });
 
     //pw must have 1 lowercase letter, 1 uppercase, 1 number and must be between 8 and 32 characters
-    if (!password.match(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])([^\s]){8,32}$/)) {
+    if (!password.match(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])([^\s]){8,}$/)) {
         res.status(400).send("failed to create user: insecure password");
         return;
     }
 
     //email address must be valid
-    if (!username.match(/^([_a-z0-9]+[\._a-z0-9]*)(\+[a-z0-9]+)?@(([a-z0-9-]+\.)*[a-z]{2,4})$/)) {
+    if (!email.match(/^([_a-z0-9]+[\._a-z0-9]*)(\+[a-z0-9]+)?@(([a-z0-9-]+\.)*[a-z]{2,4})$/)) {
         res.status(400).send("failed to create user: invalid email address");
         return;
     }
 
-    const foundUser = await User.findOne({ username: user.username }).exec();
+    const foundUser = await User.findOne({ username: user.email }).exec();
 
     if (foundUser === null) {
-        const user = await user.save();
+        const userWasSaved = (await user.save()) !== null;
         
-        if (user !== null) {
+        if (userWasSaved) {
             res.status(201).send("user saved successfully");
         } else {
             res.status(500).send("failed to create user");
@@ -35,4 +35,4 @@ const newUser = async (req, res, next) => {
     
 };
 
-module.exports = {newUser};
+module.exports = { newUser};
