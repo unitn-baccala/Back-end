3216bd74ff85287077f9399746293ce99d004253
circled-square <circled-square@github.com>
2023-02-05 16:57:23 +0100

    now using JSON for all responses

--- src/controllers/ingredient.js
+++ src/controllers/ingredient.js
@@ -3,48 +3,49 @@ const Ingredient = require('../models/ingredient')
 // POST /ingredient => createIngredient
 const createIngredient = async (req, res, next) => { 
     if (req.body == null || req.body.jwt_payload == null) {
-        res.status(400).send("failed to create ingredient");
+        res.status(400).send({ msg: "failed to create ingredient" });
         return;
     }
 
     const name = req.body.name, ownerEmail = req.body.jwt_payload.email; 
     if(name == null || String(name).length < 1) {
-        res.status(400).send("failed to create ingredient: invalid name");
+        res.status(400).send({ msg: "failed to create ingredient: invalid name" });
         return;
     }
 
     const found = (await Ingredient.findOne({ name, ownerEmail }).exec()) !== null;
 
     if(found) {
-        res.status(400).send("failed to create ingredient: name taken");
+        res.status(400).send({ msg: "failed to create ingredient: name taken" });
     } else {
         let document = new Ingredient ({ name, ownerEmail });
         const wasSaved = (await document.save()) !== null;
 
         if (wasSaved) {
-            res.status(201).send("ingredient saved successfully");
+            res.status(201).send({ msg: "ingredient saved successfully" });
         } else {
-            res.status(500).send("failed to create ingredient");
+            res.status(500).send({ msg: "failed to create ingredient" });
         }
     }
 }
 
 const deleteIngredient = async (req, res, next) => {
+    //TODO: should be checking if the ingredients is used in any dishes
     if (req.body == null) {
-        res.status(400).send("failed to delete ingredient");
+        res.status(400).send({ msg: "failed to delete ingredient" });
         return;
     }
     const name = req.body.name, ownerEmail = req.body.jwt_payload.email;
 
     if(name == null)
-        res.status(400).send("failed to delete ingredient");
+        res.status(400).send({ msg: "failed to delete ingredient" });
     else {
         let del_count = (await Ingredient.deleteOne({ name, ownerEmail })).deletedCount;
 
         if (del_count != 0) {
-            res.status(200).send("ingredient deleted successfully");
+            res.status(200).send({ msg: "ingredient deleted successfully" });
         } else {
-            res.status(400).send("failed to delete ingredient");
+            res.status(400).send({ msg: "failed to delete ingredient" });
         }
     }
 }
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,22 +4,23 @@ const argon2 = require('argon2');
 // POST /api/user => createUser
 const createUser = async (req, res, next) => {
     if (req.body == null) {
-        res.status(400).send("failed to create user");
+        res.status(400).send({ msg: "failed to create user" });
         return;
     }
 
     const password = req.body.password;
     let email = req.body.email;
+    const business_name = req.body.business_name;
 
-    if (password == null || email == null) {
-        res.status(400).send("failed to create user");
+    if (password == null || email == null || business_name == null) {
+        res.status(400).send({ msg: "failed to create user" });
         return;
     }
 
     // pw must have 1 (lower of uppercase) letter and must be between 12 and 64 characters
     // the maximum is important to protect from long password DoS attacks
     if (!password.match(/^(?=.*[a-zA-Z])(.{12,64})$/)) {
-        res.status(400).send("failed to create user: insecure password");
+        res.status(400).send({ msg: "failed to create user: insecure password" });
         return;
     }
 
@@ -28,14 +29,14 @@ const createUser = async (req, res, next) => {
 
     //email address must be valid
     if (!email.match(/^([_a-z0-9]+[\._a-z0-9]*)(\+[a-z0-9]+)?@(([a-z0-9-]+\.)*[a-z]{2,4})$/)) {
-        res.status(400).send("failed to create user: invalid email address");
+        res.status(400).send({ msg: "failed to create user: invalid email address" });
         return;
     }
 
     const wasFound = await User.findOne({ email }).exec() !== null;
     
     if (wasFound) {
-        res.status(400).send("failed to create user: username taken");
+        res.status(400).send({ msg: "failed to create user: username taken" });
     } else {
         const user = new User({
             email: req.body.email, password_hash: await argon2.hash(password)
@@ -44,9 +45,9 @@ const createUser = async (req, res, next) => {
         const wasSaved = (await user.save()) !== null;
         
         if (wasSaved) {
-            res.status(201).send("user saved successfully");
+            res.status(201).send({ msg: "user saved successfully" });
         } else {
-            res.status(500).send("failed to create user");
+            res.status(500).send({ msg: "failed to create user" });
         }
     }
 };
@@ -56,7 +57,7 @@ const deleteUser = async (req, res, next) => {
     const email = req.body.email, password = req.body.password;
 
     if(email == null || password == null)
-        res.status(400).send("failed to delete user");
+        res.status(400).send({ msg: "failed to delete user" });
     else {
         const user = await User.findOne({ email }).exec()
     
@@ -64,12 +65,12 @@ const deleteUser = async (req, res, next) => {
             let del_count = (await User.deleteOne({ email, password_hash: user.password_hash })).deletedCount;
 
             if (del_count != 0) {
-                res.status(200).send("user deleted successfully");
+                res.status(200).send({ msg: "user deleted successfully" });
             } else {
-                res.status(500).send("failed to delete user");
+                res.status(500).send({ msg: "failed to delete user" });
             }
         } else {
-            res.status(400).send("failed to delete user");
+            res.status(400).send({ msg: "failed to delete user" });
         }
     }
 }
