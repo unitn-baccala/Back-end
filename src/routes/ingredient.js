4740926c30d7147028a4bca31faed240f23cbfc1
Kalsifer <kalsifer742@gmail.com>
2023-02-05 17:40:49 +0100

    spostato il middleware per il controllo del jwt dal livello applicazione al router

--- src/routes/ingredient.js
+++ src/routes/ingredient.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const tokenChecker = require('../functions/tokenChecker');
 const controller = require('../controllers/ingredient');
 
-router.post('/ingredient', controller.createIngredient);
-router.delete('/ingredient', controller.deleteIngredient);
+router.post('/ingredient', tokenChecker, controller.createIngredient);
+router.delete('/ingredient', tokenChecker, controller.deleteIngredient);
 router.get('/ingredient', controller.getIngredients);
 
 module.exports = router;
