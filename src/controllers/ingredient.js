425890934436852abc514125f3cf8a9f4e77b9c5
Kalsifer <kalsifer742@gmail.com>
2023-02-06 19:28:14 +0100

    modificato il funzionamento della funzione fail

--- src/controllers/ingredient.js
+++ src/controllers/ingredient.js
@@ -2,29 +2,28 @@ const Ingredient = require('../models/ingredient');
 const User = require('../models/user');
 const fail = require('../functions/fail');
 
-// POST /ingredient => createIngredient
 const createIngredient = async (req, res, next) => { 
     const failMessage = "failed to create ingredient: "
 
     /* istanbul ignore next */
     if (req.body == null || req.body.jwt_payload == null) //this should never happen since the API requires token checking 
-        return fail(500, failMessage, "req.body == null || req.body.token == null");
+        return fail(res, 500, failMessage, "req.body == null || req.body.token == null");
 
     const name = req.body.name, owner_id = req.body.jwt_payload.user_id; 
     if(name == null || String(name).length < 1)
-        return fail(400, failMessage, "invalid_name");
+        return fail(res, 400, failMessage, "invalid_name");
 
     const found = (await Ingredient.findOne({ name, owner_id }).exec()) !== null;
 
     if(found) 
-        return fail(400, failMessage, "name taken");
+        return fail(res, 400, failMessage, "name taken");
 
     let document = new Ingredient ({ name, owner_id });
     const was_saved = (await document.save()) !== null;
 
     /* istanbul ignore next */
     if(!was_saved)
-        return fail(500, failMessage, "internal server error")
+        return fail(res, 500, failMessage, "internal server error")
 
     res.status(201).send({ msg: "ingredient saved successfully" });
 }
@@ -35,16 +34,16 @@ const deleteIngredient = async (req, res, next) => {
  
     /* istanbul ignore next */
     if (req.body == null || req.body.jwt_payload == null) // the function is token checked so this cannot happen
-        return fail(500, failMessage, "req.body == null || req.body.token == null");
+        return fail(res, 500, failMessage, "req.body == null || req.body.token == null");
     
     const name = req.body.name, owner_id = req.body.jwt_payload.user_id;
 
     if(name == null)
-        return fail(400, failMessage, "req.body.name == null");
+        return fail(res, 400, failMessage, "req.body.name == null");
 
     let del_count = (await Ingredient.deleteOne({ name, owner_id })).deletedCount;
     if(del_count == 0)
-        return fail(400, failMessage, "no ingredient with such name");
+        return fail(res, 400, failMessage, "no ingredient with such name");
 
     res.status(200).send({ msg: "ingredient deleted successfully" });
 }
@@ -54,19 +53,19 @@ const getIngredients = async (req, res, next) => {
 
     /* istanbul ignore next */
     if (req.query == null) //req.query can be empty but not null so this should not happen
-        return fail(400, failMessage, "no parameters");
+        return fail(res, 400, failMessage, "no parameters");
     
     const business_name = req.query.business_name;
     if(business_name == null)
-        return fail(400, failMessage, "no business name specified");
+        return fail(res, 400, failMessage, "no business name specified");
 
     const user = await User.findOne({business_name});
     if(!user)
-        return fail(400, failMessage, "no such business name found");
+        return fail(res, 400, failMessage, "no such business name found");
 
     const ingredients = await Ingredient.find({ owner_id: user._id });
     if (!ingredients)
-        return fail(400, failMessage, "no ingredients found");
+        return fail(res, 400, failMessage, "no ingredients found");
     
     res.status(200).send(ingredients);
 }
